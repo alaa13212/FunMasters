533bdc717d8529b82e147a9a5962ead8b003b8fc
Ali Albarrak <alaa13212@gmail.com>
2025-11-25 13:28:09 +0300

    Allow reviewing active game. Closes #5

---
+++ FunMasters/FunMasters/Models/RatingRecord.cs
@@ -0,0 +1,11 @@
+﻿using FunMasters.Data;
+
+namespace FunMasters.Models;
+
+public class RatingRecord(Suggestion game, RatingValue score = RatingValue.Perfect, string? comment = null)
+{
+    public Suggestion Game { get; init; } = game;
+    public RatingValue Score { get; set; } = score;
+    public string? Comment { get; set; } = comment;
+        
+}
