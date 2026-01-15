d357cd7976bf9f66280ea242c53592ad76a722c2
DerpWyvern <ice1breaker@gmail.com>
2026-01-16 02:36:28 +0300

    added steam links to games

--- FunMasters/FunMasters/Services/IgdbGame.cs
+++ FunMasters/FunMasters/Services/IgdbGame.cs
@@ -4,5 +4,12 @@ public class IgdbGame
 {
     public int id { get; set; }
     public string name { get; set; } = string.Empty;
-    public int? cover { get; set; }
+    public IgdbLink cover { get; set; }
+    public List<IgdbLink> websites { get; set; }
+}
+
+public class IgdbLink
+{
+    public int id { get; set; }
+    public string url { get; set; }
 }
