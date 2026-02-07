7c5ec427256df2e97c2a8ab21152fb57125caee8
Ali Albarrak <alaa13212@gmail.com>
2026-02-07 15:14:16 +0300

    Update rating system

--- FunMasters/FunMasters/Models/RatingRecord.cs
+++ FunMasters/FunMasters/Models/RatingRecord.cs
@@ -1,11 +1,14 @@
 ﻿using FunMasters.Data;
+using FunMasters.Services;
 
 namespace FunMasters.Models;
 
-public class RatingRecord(Suggestion game, RatingValue score = RatingValue.Perfect, string? comment = null)
+public class RatingRecord(Suggestion game, decimal score = 10m, string? comment = null)
 {
     public Suggestion Game { get; init; } = game;
-    public RatingValue Score { get; set; } = score;
+    public decimal Score { get; set; } = score;
     public string? Comment { get; set; } = comment;
-        
+    
+    public string GetRatingLabel() => RatingUtils.GetRatingLabel((int)(Score * 10));
+
 }
