405aa8cdcdca17462bcb77100fd86a52ea94a404
Ali Albarrak <alaa13212@gmail.com>
2026-02-12 02:38:03 +0300

    Enable response/request compression, update project properties, and enforce `Roles` as required in `LoggedInUserInfo`.

--- FunMasters/FunMasters.Shared/DTOs/LoggedInUserInfo.cs
+++ FunMasters/FunMasters.Shared/DTOs/LoggedInUserInfo.cs
@@ -8,5 +8,5 @@ public class LoggedInUserInfo
     public required string Email { get; set; }
     public required string DisplayName { get; set; }
     public required string AvatarTimestamp { get; set; }
-    public List<string> Roles { get; set; }
+    public required List<string> Roles { get; set; }
 }
